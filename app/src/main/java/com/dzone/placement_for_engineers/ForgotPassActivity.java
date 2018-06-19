ed5bca4f5eead405fedca06d839d55a53af0e0a8
Kartik Chauhan <lkchauhan.725@gmail.com>
2018-06-19 14:31:06 +0530

    coded for forgot pass and direct main page will be opened if user is already logged in

--- app/src/main/java/com/dzone/placement_for_engineers/ForgotPassActivity.java
+++ app/src/main/java/com/dzone/placement_for_engineers/ForgotPassActivity.java
@@ -1,24 +1,34 @@
 package com.dzone.placement_for_engineers;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 import org.w3c.dom.Text;
 
 public class ForgotPassActivity extends AppCompatActivity {
     EditText email;
     Button reset;
+    FirebaseAuth mAuth;
     TextView back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_pass);
+
+        mAuth = FirebaseAuth.getInstance();
 
         email = (EditText)findViewById(R.id.forgot_pass_email);
         reset = (Button)findViewById(R.id.forgot_pass_reset);
@@ -27,7 +37,28 @@ public class ForgotPassActivity extends AppCompatActivity {
         reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String emailtxt = email.getText().toString().trim();
 
+                if (TextUtils.isEmpty(emailtxt)) {
+                    Toast.makeText(getApplication(), "Enter your registered email id", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+                mAuth.sendPasswordResetEmail(emailtxt)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(ForgotPassActivity.this, "We have sent you instructions to reset your password!", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(ForgotPassActivity.this,LoginActivity.class);
+                                    startActivity(intent);
+                                } else {
+                                    Toast.makeText(ForgotPassActivity.this, "Failed to send reset email!", Toast.LENGTH_SHORT).show();
+                                }
+
+                            }
+                        });
             }
         });
 
--- app/src/main/java/com/dzone/placement_for_engineers/WelcomeActivity.java
+++ app/src/main/java/com/dzone/placement_for_engineers/WelcomeActivity.java
@@ -1,18 +1,43 @@
 package com.dzone.placement_for_engineers;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class WelcomeActivity extends AppCompatActivity {
     private Button login,signup;
+    FirebaseAuth.AuthStateListener mAuthListener;
+    GoogleSignInClient mGoogleSignInClient;
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser() != null){
+                    startActivity(new Intent(WelcomeActivity.this,MainPageActivity.class));
+                }
+            }
+        };
 
         login = (Button)findViewById(R.id.welcome_login);
         signup = (Button)findViewById(R.id.welcome_signup);
