d52df33003cab348b60d6f3a929008a134dee1cd
Sagar <Sagarrishu@gmail.com>
2018-06-26 00:03:01 +0530

    changed listview to cardview..test2

--- app/src/main/java/com/dzone/placement_for_engineers/CompaniesTechFragment.java
+++ app/src/main/java/com/dzone/placement_for_engineers/CompaniesTechFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -45,10 +46,12 @@ public class CompaniesTechFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_companies_tech, container, false);
+        recyclerView = getActivity().findViewById(R.id.tech_list_recycler);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser user = mAuth.getCurrentUser();
         databaseReference = database.getReference("Companies").child("Tech");
-        lvw = (ListView)view.findViewById(R.id.tech_list);
         listItems = new ArrayList<>();
 
         ValueEventListener eventListener = new ValueEventListener() {
