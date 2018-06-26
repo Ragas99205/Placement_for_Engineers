221223ac604f08ada1ca6f6f49d6f732c00afeaa
Melburne Rodrigues <melburne42@gmail.com>
2018-06-26 13:30:09 +0530

    Hiring process completed

---
+++ app/src/main/java/com/dzone/placement_for_engineers/HiringProcProductBased.java
@@ -0,0 +1,13 @@
+package com.dzone.placement_for_engineers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HiringProcProductBased extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hiring_proc_product_based);
+    }
+}
--- app/src/main/java/com/dzone/placement_for_engineers/HiringProcTechFragment.java
+++ app/src/main/java/com/dzone/placement_for_engineers/HiringProcTechFragment.java
@@ -1,6 +1,7 @@
 package com.dzone.placement_for_engineers;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -9,12 +10,39 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class HiringProcTechFragment extends Fragment {
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_hiring_proc_tech, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_hiring_proc_tech, container,false);
+
+        Button product = (Button)v.findViewById(R.id.product);
+        Button service = (Button)v.findViewById(R.id.service);
+
+        product.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent productIntent = new Intent(getActivity(), HiringProcProductBased.class);
+                startActivity(productIntent);
+            }
+        });
+
+        service.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent serviceIntent = new Intent(getActivity(), HiringProcServiceBased.class);
+                startActivity(serviceIntent);
+            }
+        });
+
+        return v;
     }
+
 }
+
